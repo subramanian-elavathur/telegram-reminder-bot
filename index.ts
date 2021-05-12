deeac7d7e8f8f2fede16bb589e6747faede33d24
Subramanian Elavathur <subramanian.elavathur@gmail.com>
2021-05-12 22:46:55 +0530

    Async countdown messages from bot

--- index.ts
+++ index.ts
@@ -5,6 +5,17 @@ bot.start((ctx) => ctx.reply("Hello There"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.hears("count me down", (ctx) => {
+  let counter = 5;
+  let clr = setInterval(() => {
+    bot.telegram.sendMessage(ctx.message.chat.id, counter);
+    counter = counter - 1;
+    if (counter === 0) {
+      bot.telegram.sendMessage(ctx.message.chat.id, "I did as you asked");
+      clearInterval(clr);
+    }
+  }, 1000);
+});
 bot.launch();
 
 // graceful stopping
