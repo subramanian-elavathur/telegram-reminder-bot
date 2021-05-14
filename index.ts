4f4f345d03201091f87e778163f04ccd638fc8fd
Subramanian Elavathur <subramanian.elavathur@gmail.com>
2021-05-14 21:49:31 +0530

    Integrate Keyboard Markups to Create user journey

--- index.ts
+++ index.ts
@@ -2,16 +2,29 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply("Hello There"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 bot.command("lucky", (ctx) => {
-  bot.telegram.sendDice(ctx.message.chat.id);
+  bot.telegram.sendDice(ctx.message.chat.id, {
+    reply_markup: { remove_keyboard: true },
+  });
 });
+bot.command("sup", (ctx) => {
+  bot.telegram.sendMessage(
+    ctx.message.chat.id,
+    "So what would you like to do today",
+    {
+      reply_markup: {
+        keyboard: [["/countdown"], ["/lucky"]],
+      },
+    }
+  );
+});
+
 bot.command("countdown", (ctx) => {
   let counter = 5;
   let clr = setInterval(() => {
-    bot.telegram.sendMessage(ctx.message.chat.id, counter);
+    bot.telegram.sendMessage(ctx.message.chat.id, counter, {
+      reply_markup: { remove_keyboard: true },
+    });
     counter = counter - 1;
     if (counter === 0) {
       bot.telegram.sendMessage(ctx.message.chat.id, "I did as you asked");
