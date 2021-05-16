fa7317224e0d7599d37c946bf7314acdd1ccc88a
Subramanian Elavathur <subramanian.elavathur@gmail.com>
2021-05-16 20:44:17 +0530

    Convert to inline keybaord

--- index.ts
+++ index.ts
@@ -2,36 +2,58 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply("Hello There"));
-bot.command("lucky", (ctx) => {
-  bot.telegram.sendDice(ctx.message.chat.id, {
-    reply_markup: { remove_keyboard: true },
-  });
+bot.on("callback_query", (ctx) => {
+  switch (ctx.callbackQuery.data) {
+    case "lucky":
+      makeLucky(ctx);
+      break;
+    case "countdown":
+      startCountdown(ctx);
+      break;
+    default:
+      break;
+  }
+  ctx.answerCbQuery();
 });
+
 bot.command("sup", (ctx) => {
   bot.telegram.sendMessage(
     ctx.message.chat.id,
     "So what would you like to do today",
     {
       reply_markup: {
-        keyboard: [["/countdown"], ["/lucky"]],
+        inline_keyboard: [
+          [
+            { text: "countdown", callback_data: "countdown" },
+            { text: "lucky", callback_data: "lucky" },
+          ],
+        ],
       },
     }
   );
 });
 
-bot.command("countdown", (ctx) => {
+const makeLucky = (ctx) => {
+  bot.telegram.sendDice(ctx.callbackQuery.message.chat.id, {
+    reply_markup: { remove_keyboard: true },
+  });
+};
+
+const startCountdown = (ctx) => {
   let counter = 5;
   let clr = setInterval(() => {
-    bot.telegram.sendMessage(ctx.message.chat.id, counter, {
-      reply_markup: { remove_keyboard: true },
-    });
+    bot.telegram.sendMessage(ctx.callbackQuery.message.chat.id, counter);
     counter = counter - 1;
     if (counter === 0) {
-      bot.telegram.sendMessage(ctx.message.chat.id, "I did as you asked");
+      bot.telegram.sendMessage(
+        ctx.callbackQuery.message.chat.id,
+        "I did as you asked"
+      );
       clearInterval(clr);
     }
   }, 1000);
-});
+};
+
 bot.launch();
 
 // graceful stopping
