c1d2a7ab160039b06ba14227b0c2e9cd6b5c8f74
Subramanian Elavathur <subramanian.elavathur@gmail.com>
2021-05-19 22:08:58 +0530

    Adding simple 10 second reminder

--- index.ts
+++ index.ts
@@ -1,20 +1,9 @@
 const { Telegraf } = require("telegraf");
 
+const draftChatReminders = new Set();
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply("Hello There"));
-bot.on("callback_query", (ctx) => {
-  switch (ctx.callbackQuery.data) {
-    case "lucky":
-      makeLucky(ctx);
-      break;
-    case "countdown":
-      startCountdown(ctx);
-      break;
-    default:
-      break;
-  }
-  ctx.answerCbQuery();
-});
 
 bot.command("sup", (ctx) => {
   bot.telegram.sendMessage(
@@ -26,12 +15,50 @@ bot.command("sup", (ctx) => {
           [
             { text: "countdown", callback_data: "countdown" },
             { text: "lucky", callback_data: "lucky" },
+            { text: "reminder", callback_data: "reminder" },
           ],
         ],
       },
     }
   );
 });
+
+bot.on("callback_query", (ctx) => {
+  switch (ctx.callbackQuery.data) {
+    case "lucky":
+      makeLucky(ctx);
+      break;
+    case "countdown":
+      startCountdown(ctx);
+      break;
+    case "reminder":
+      remind(ctx);
+      break;
+    default:
+      break;
+  }
+  ctx.answerCbQuery();
+});
+
+bot.on("message", (ctx) => {
+  const chatId = ctx.message.chat.id;
+  if (draftChatReminders.has(chatId)) {
+    setTimeout(() => {
+      bot.telegram.sendMessage(chatId, `REMINDER - ${ctx.message.text}`);
+    }, 10000);
+    bot.telegram.sendMessage(chatId, "Reminder set! Will remind in 10 sec");
+    draftChatReminders.delete(chatId);
+  }
+});
+
+const remind = (ctx) => {
+  const chatId = ctx.callbackQuery.message.chat.id;
+  draftChatReminders.add(chatId);
+  bot.telegram.sendMessage(
+    ctx.callbackQuery.message.chat.id,
+    "what would you like to be reminded of?"
+  );
+};
 
 const makeLucky = (ctx) => {
   bot.telegram.sendDice(ctx.callbackQuery.message.chat.id, {
