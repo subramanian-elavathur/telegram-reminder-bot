0337a4f100b7b9f34c762c9f63cf324eaeccd861
Subramanian Elavathur <subramanian.elavathur@gmail.com>
2021-06-07 21:36:19 +0530

    define required recurrence usecases

---
+++ recurrence.ts
@@ -0,0 +1,6 @@
+const examples = [
+  "every day at hh:mm:ss",
+  "every week on [M|T|W|T|F|S|S].* at hh:mm:ss",
+  "every month on (the d+)? [(first|second|third|fourth|fifth) (M|T|W|T|F|S|S]) at hh:mm:ss",
+  "every year on (the d+) | ([first-fifth] [day]) of [month] at hh:mm:ss",
+];
