e2413327c9e6d2d136d4752c198686b0206ef805
Subramanian Elavathur <subramanian.elavathur@gmail.com>
2021-07-04 10:58:32 +0530

    View my reminders - introduce id and active status

--- tracker.ts
+++ tracker.ts
@@ -1,12 +1,25 @@
 import { SimpleLocalDB } from "./local-db";
 
 interface Tracker {
+  id: number;
   duration: number;
   reminderText: string;
   timezone: string;
+  active: boolean;
 }
 
 const tracker = new SimpleLocalDB<Tracker[]>(process.env.TRACKER_DB_DIRECTORY);
+const id = new SimpleLocalDB<number>(`${process.env.TRACKER_DB_DIRECTORY}/id`);
+
+const getNextId = async (user: string): Promise<number> => {
+  const existingId = await id.get(user);
+  let nextId = 1;
+  if (existingId > 0) {
+    nextId = existingId + 1;
+  }
+  await id.set(user, nextId);
+  return Promise.resolve(nextId);
+};
 
 export const updateTracker = async (
   user: string,
@@ -16,15 +29,18 @@ export const updateTracker = async (
 ): Promise<boolean> => {
   try {
     const existingTrackersForUser = await tracker.get(user);
-    const newTrackers = durations.map((each) => ({
-      duration: each,
-      reminderText: reminderText,
-      timezone,
-    }));
     const updatedTrackers =
-      existingTrackersForUser?.length > 0
-        ? [...existingTrackersForUser, ...newTrackers]
-        : newTrackers;
+      existingTrackersForUser?.length > 0 ? [...existingTrackersForUser] : [];
+    for (const duration of durations) {
+      const nextId = await getNextId(user);
+      updatedTrackers.push({
+        id: nextId,
+        reminderText: reminderText,
+        active: false,
+        duration,
+        timezone,
+      });
+    }
     await tracker.set(user, updatedTrackers);
     return Promise.resolve(true);
   } catch (e) {
