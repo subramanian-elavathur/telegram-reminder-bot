4795e49b8ee0d4f896040017c8188d8b4df2e0a2
Subramanian Elavathur <subramanian.elavathur@gmail.com>
2021-09-05 19:29:57 +0530

    Upgrade to v0.13.0

--- tests/reminder.test.ts
+++ tests/reminder.test.ts
@@ -5,17 +5,17 @@ import { remindClause } from "../src/reminders";
 const now = Settings.now;
 const { before, test, after } = group("reminders");
 
-before((done, log) => {
-  log("Updating Luxon now method to return static date");
+before((c) => {
+  c.log("Updating Luxon now method to return static date");
   Settings.now = () => new Date(Date.UTC(0, 0, 0, 0, 0, 0)).valueOf();
-  done();
+  c.done();
 });
 
-test("In Clause 1", (verify) =>
-  verify.check(7200000, remindClause("in 2 hours", "Asia/Kolkata")[0]).done());
+test("In Clause 1", (c) =>
+  c.check(7200000, remindClause("in 2 hours", "Asia/Kolkata")[0]).done());
 
-test("In Clause 2", (v) =>
-  v
+test("In Clause 2", (c) =>
+  c
     .check(7320000, remindClause("in 2 hours and 2 minutes", "Asia/Kolkata")[0])
     .done());
 
@@ -25,7 +25,7 @@ test("In Clause 2", (v) =>
 //     .check(99530000, remindClause("On 1-1-1900 at 09:00", "Asia/Kolkata")[0])
 //     .done());
 
-after((done) => {
+after((c) => {
   Settings.now = now;
-  done();
+  c.done();
 });
