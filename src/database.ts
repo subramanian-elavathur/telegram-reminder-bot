f137c4ad1326b6f7df6921c8745243179c7c8499
Subramanian Elavathur <subramanian.elavathur@gmail.com>
2021-10-02 11:59:00 +0530

    disable caching on glitch-db

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import GlitchDB from "glitch-db";
 
-const GLITCH_DB_CACHE_SIZE = 1000;
+const GLITCH_DB_CACHE_SIZE = 0;
 
 const database = new GlitchDB(process.env.DB_DIRECTORY, GLITCH_DB_CACHE_SIZE);
 
